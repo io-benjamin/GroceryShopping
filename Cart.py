4fba42bc66d5831f3fc5fc76aa322c87cc1e3608
Benjamin Estrada <75432075+io-benjamin@users.noreply.github.com>
2023-09-25 22:50:28 -0400

    new

--- Cart.py
+++ Cart.py
@@ -3,33 +3,31 @@ import requests
 
 app = Flask(__name__)
 
-PRODUCT_SERVICE_URL = "https://products-service-ift9.onrender.com"
+# PRODUCT_SERVICE_URL = "https://products-service-ift9.onrender.com"
+PRODUCT_SERVICE_URL = "http://localhost:5001"
 
-shopping_cart = {}
+users = ["user1", "user2"]
 
-def get_product_details(product_id):
-    response = requests.get(f"{PRODUCT_SERVICE_URL}/products/{product_id}")
-    if response.status_code == 200:
-        return response.json()
-    return None 
+shopping_cart = {user_id: {} for user_id in users}
 
-@app.route('/cart/<int:user_id>', methods=['GET'])
+@app.route('/cart/<string:user_id>', methods=['GET'])
 def get_cart(user_id):
     if user_id not in shopping_cart:
         return jsonify({"error": "Cart not found"}), 404
     cart = shopping_cart[user_id]
     return jsonify(cart)
 
-@app.route('/cart/<int:user_id>/add/<int:product_id>', methods=['POST'])
+@app.route('/cart/<string:user_id>/add/<int:product_id>', methods=['POST'])
 def add_to_cart(user_id, product_id):
     data = request.json
     if 'quantity' not in data: 
         return jsonify({"error": "Quantity not specified"}), 400
     
-    product = get_product_details(product_id)
-    if not product:
+    product_response = requests.get(f"{PRODUCT_SERVICE_URL}/products/{product_id}")
+    if product_response.status_code != 200:
         return jsonify({"error": "Product not found"}), 404
     
+    product = product_response.json()
     quantity_to_add = data['quantity']
     
     if user_id not in shopping_cart:
@@ -37,17 +35,17 @@ def add_to_cart(user_id, product_id):
         
     cart = shopping_cart[user_id]
     
-    if product_id not in cart:
-        cart[product_id]['quantity'] += quantity_to_add
-    else: 
-        cart[product_id] = {
-            'product_name': product['name'],
-            'quantity': quantity_to_add,
-            'total_price': product['price'] * quantity_to_add
-        }
+    if product_id in cart:
+       cart[product_id]['quantity'] += quantity_to_add
+    else:
+       cart [product_id] = {
+                "name": product['name'],
+                "quantity": quantity_to_add,
+                "total_price": product['price'] * quantity_to_add
+       }
     return jsonify({"message": "Product added to cart"}), 201
 
-@app.route('/cart/<int:user_id>/remove/<int:product_id>', methods=['POST'])
+@app.route('/cart/<string:user_id>/remove/<int:product_id>', methods=['POST'])
 def remove_from_cart(user_id, product_id):
     data = request.json
     if 'quantity' not in data: 
@@ -61,14 +59,17 @@ def remove_from_cart(user_id, product_id):
         return jsonify({"error": "Product not found in cart"}), 404
     
     quantity_to_remove = data['quantity']
+    
     if cart[product_id]['quantity'] <= quantity_to_remove:
         del cart[product_id]
     else:
         cart[product_id]['quantity'] -= quantity_to_remove
-        cart[product_id]['total_price'] -= get_product_details(product_id)['price'] * quantity_to_remove
+        
+        product = get_product_details(product_id)
+        cart[product_id]['total_price'] -= product['price'] * quantity_to_remove
     
     return jsonify({"message": "Product removed from cart"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
 
--- Product.py
+++ Product.py
@@ -4,14 +4,17 @@ app = Flask(__name__)
 
 products = [
     {"id": 1, "name": "Apple", "price": 1.0, "quantity": 100},
-    {"id": 2, "name": "Banana", "price": 0.5, "quantity": 50}
+    {"id": 2, "name": "Banana", "price": 0.5, "quantity": 50},
+    {"id": 3, "name": "Orange", "price": 1.5, "quantity": 75},
+    {"id": 4, "name": "Pineapple", "price": 3.0, "quantity": 25},
+    {"id": 5, "name": "Mango", "price": 2.0, "quantity": 125},
 ]
 
 @app.route('/products', methods=['GET'])
 def get_products():
     return jsonify(products)
 
-@app.route('/products/<int:product_id>', methods=['GET'])
+@app.route('/products/<int:products_id>', methods=['GET'])
 def get_product(product_id):
     product = next((p for p in products if p['id'] == product_id), None)
     if product: 
@@ -22,15 +25,17 @@ def get_product(product_id):
 def add_product():
     data = request.json
     if 'name' not in data or 'price' not in data or 'quantity' not in data:
-        new_product = {
-            "id": len(products) + 1,
-            "name": data['name'],
-            "price": data['price'],
-            "quantity": data['quantity']
-        }
-        products.append(new_product)
-        return jsonify(new_product), 201
-    return jsonify({"error": "Product not added"}), 400
+        return jsonify({"error": "Invalid product data"}), 400
+    
+    new_product = {
+        "id": len(products) + 1,
+        "name": data['name'],
+        "price": data['price'],
+        "quantity": data['quantity']
+    }
+    products.append(new_product)
+        
+    return jsonify(new_product), 201
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
